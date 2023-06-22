0857d01fa2429f2d29ba0e753b2b264927a60181
drakem58 <mdrake58@protonmail.com>
2023-06-22 13:06:25 -0400

    Add files via upload

---
+++ terraform2/terraform-aws-server-main/examples/attach-eni/user-data/user-data-1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# This script is meant to run in the User Data of an EC2 instance to attach an ENI by ID
+
+set -e
+
+# Send the log output from this script to user-data.log, syslog, and the console
+# From: https://alestic.com/2010/12/ec2-user-data-output/
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+# Attach the ENI
+/usr/local/bin/attach-eni --eni-id "${eni_id_a}" --device-index 1
+/usr/local/bin/attach-eni --eni-id "${eni_id_b}" --device-index 2
+
+# We start a web server so that we can make an HTTP request to the ENI IP address to validate that it's working correctly
+echo "Starting server on port ${server_port} that returns text ${server_text}"
+echo "${server_text}" > index.html
+
+nohup python3 -m http.server "${server_port}" 2>&1 | logger &
